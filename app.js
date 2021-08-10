edc1941c9b4c7b77efe34de9136e42cba7da2853
tonymaung <tonymaung01@gmail.com>
2021-08-10 14:21:31 +0630

    cmt added

--- app.js
+++ app.js
@@ -1,28 +1,6 @@
 const button = document.getElementById("button")
 const audioElement = document.getElementById("audio");
 
-//get Jokes from Joke URL
-
-async function getJokes() {
-    let text = ''
-    try {
-        const response = await fetch("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit")
-        const data = await response.json()
-        if (data.setup) {
-            text = `${data.setup} ... ${data.delivery}`
-        } else {
-            text = data.joke;
-        }
-        //text to Speech
-        tellMeJoke(text)
-            //Toggle button
-        toggleButton()
-    } catch (err) {
-        console.log("Fetch failed", err)
-    }
-}
-
-
 //Passing Joke text to speech 
 function tellMeJoke(text) {
     let textToSpeech = text.trim().replace(/ /g, '%20');
@@ -38,10 +16,34 @@ function tellMeJoke(text) {
         ssml: false
     })
 }
-
+//Toggle Button 
 function toggleButton() {
     button.disabled = !button.disabled
 }
-//Event Listener
-button.addEventListener("click", getJokes);
-audioElement.addEventListener("ended", toggleButton)
+//get Jokes from Joke URL
+
+async function getJokes() {
+    let text = ''
+    try {
+        //fetch data from URL
+        const response = await fetch("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit")
+            //convert it into JSON
+        const data = await response.json()
+            //Check whether two part or not
+        if (data.setup) {
+            text = `${data.setup} ... ${data.delivery}`
+        } else {
+            text = data.joke;
+        }
+        //text to Speech
+        tellMeJoke(text)
+            //Disable button while playing audio
+        toggleButton()
+    } catch (err) {
+        console.log("Fetch failed", err)
+    }
+}
+
+//Event Listeners
+button.addEventListener("click", getJokes); //Click to get new jokes
+audioElement.addEventListener("ended", toggleButton) //enable button after the audio is played
